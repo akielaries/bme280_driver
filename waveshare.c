98b6f179ace84feed895861f3ab04f6da0f9dee0
akielaries <akiel@akiel.org>
2023-09-30 16:17:39 +0000

    updating `waveshare.c`

--- waveshare.c
+++ waveshare.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "lib/util.h"
 
+
+
 int main() {
     // I2C device
     const char* i2c_bus = "/dev/i2c-1";
@@ -12,11 +14,7 @@ int main() {
     unsigned char sensor_addrs[] = {0x76, 0x68, 0x29, 0x53, 0x28};
 
     // pass in the I2C device, array of addresses, and the number of addresses
-    if (i2c_init(i2c_bus, sensor_addrs, sizeof(sensor_addrs))) {
-        printf("Initialization complete.\n");
-    } else {
-        printf("Initialization failed.\n");
-    }
+    i2c_init(i2c_bus, sensor_addrs, sizeof(sensor_addrs));
 
     return 0;
 }
