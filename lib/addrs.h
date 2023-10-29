78c4cede67280649d83c281e63ebee1bd44d1259
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2023-10-28 20:33:13 -0700

    Update addrs.h

--- lib/addrs.h
+++ lib/addrs.h
@@ -5,11 +5,11 @@
 /**
  * WaveShare board I2C sensor addresses
  */
-#define BME280_ADDR 0x76
-#define ICM20948_ADDR 0x68
-#define LTR390_ADDR 0x53
-#define SGP40_ADDR 0x28
-#define TSL2591_ADDR 0x29
+#define BME280_ADDR     0x76
+#define ICM20948_ADDR   0x68
+#define LTR390_ADDR     0x53
+#define SGP40_ADDR      0x28
+#define TSL2591_ADDR    0x29
 
 typedef struct HWInfo {
     const char *i2c_bus;
